5eb308b96a78a95513a4ef99ab845b7dce2d047f
Andrey Turubchuk <antur1977@rambler.ru>
2018-01-17 23:14:28 +0300

    add Dish

---
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/AppController.java
@@ -0,0 +1,79 @@
+package ru.topjava.restaurant.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.topjava.restaurant.model.Dish;
+
+import java.util.List;
+
+
+@RestController
+//@RequestMapping(value = "/")
+public class AppController {
+
+    @RequestMapping({"/"})
+    public String dishList() {
+        Dish dish = new Dish("картошка", 3.0);
+        return dish.toString();
+       // return dish;
+        //List<Person> personList = personService.getAll();
+        //model.addAttribute("personList", personList);
+        //return "personList";
+    }
+
+  /*  @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable long id) {
+        personService.delete(id);
+        return "redirect:/person/list";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return "personAdd";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(Person person) {
+       personService.savePerson(person);
+        return "redirect:/person/list";
+ }
+
+    @ExceptionHandler(MailException.class)
+    public String err(Model model, Exception ex){
+        model.addAttribute("texterror", ex.getMessage());
+        return "error";
+    }
+
+    @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
+    public String view(@PathVariable Long id, Model model) {
+        Person person = personService.getOne(id);
+        List<Letter> letterList = letterService.findByTo(person.getEmailAddress());
+        model.addAttribute("personLetterList", letterList);
+        model.addAttribute("personFullName", person.getFirstName() + " " + person.getLastName());
+        return "personLetterList";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable Long id, Model model) {
+        Person person = personService.getOne(id);
+        model.addAttribute("person", person);
+        return "personEdit";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
+    public String edit(@PathVariable Long id, Person personDetails) {
+        personService.editPerson(id, personDetails);
+        return "redirect:/person/list";
+    }
+
+    @RequestMapping(value = "/")
+    public String main() {
+        return "personList";
+    }*/
+}
+
+
+
