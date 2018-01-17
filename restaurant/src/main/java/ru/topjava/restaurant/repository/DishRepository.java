5e3405b17d664945d2ab0128caf5855c155ad245
Andrey Turubchuk <antur1977@rambler.ru>
2018-01-18 01:38:55 +0300

    add repository Dish and RestaurantMenu

---
+++ restaurant/src/main/java/ru/topjava/restaurant/repository/DishRepository.java
@@ -0,0 +1,13 @@
+package ru.topjava.restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+import ru.topjava.restaurant.model.Dish;
+
+import java.util.List;
+
+@Repository
+public interface DishRepository extends JpaRepository<Dish, Long> {
+    //List<Letter> findByToOrderByTo(String to);
+}
