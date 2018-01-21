a77a2e14636f66205672aba30e6658f7170204d2
Andrey Turubchuk <antur1977@rambler.ru>
2018-01-22 00:31:01 +0300

    add RestaurantController

---
+++ restaurant/src/main/java/ru/topjava/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,13 @@
+package ru.topjava.restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.topjava.restaurant.model.Restaurant;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    Restaurant findRestaurantByRestaurantId(Long id);
+    Restaurant findRestaurantByRating(Long raiting);
+
+
+}
+
+
