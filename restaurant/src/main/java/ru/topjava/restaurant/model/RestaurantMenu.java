d5678bb544da9274745d4b7bb6b5a1c302302cf5
Andrey Turubchuk <antur1977@rambler.ru>
2018-01-25 13:25:20 +0300

    add RestaurantMenu

--- restaurant/src/main/java/ru/topjava/restaurant/model/RestaurantMenu.java
+++ restaurant/src/main/java/ru/topjava/restaurant/model/RestaurantMenu.java
@@ -9,7 +9,7 @@ public class RestaurantMenu {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "dish_id")
@@ -27,11 +27,11 @@ public class RestaurantMenu {
         this.restaurant = restaurant;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
