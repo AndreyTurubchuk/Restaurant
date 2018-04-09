0750a7e378573a7b541782309d8bb43516d4cfae
Andrey Turubchuk <antur1977@rambler.ru>
2018-04-09 22:06:06 +0300

    add get mapping restaurant

--- restaurant/src/main/java/ru/topjava/restaurant/controller/RestaurantController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/RestaurantController.java
@@ -3,13 +3,50 @@ package ru.topjava.restaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.topjava.restaurant.model.Restaurant;
+import ru.topjava.restaurant.repository.RestaurantRepository;
+import ru.topjava.restaurant.restaurantMenu.RestaurantMenu;
 //import ru.topjava.restaurant.model.Restaurant;
 //import ru.topjava.restaurant.service.RestaurantService;
 
 import java.util.List;
 
-/*@RestController
+
+@Controller
+@RequestMapping(value = "restaurant")
+//@RequestMapping(value = "restaurant/restaurant/api/v1")
+public class RestaurantController {
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+/*    @GetMapping(value = "/restaurants")
+    public List<Restaurant>  restaurants() {
+        return restaurantRepository.findAll();
+    } // лист*/
+
+    @GetMapping(value = "/restaurant/add") //создание
+    public String restaurantAdd(Model model) {
+        model.addAttribute("restaurant", new Restaurant());
+        return "restaurantAddForm";
+        //return "test";
+    }
+
+    @PostMapping("/restaurant/add")
+    public String restaurantSubmit(@ModelAttribute Restaurant restaurant) {
+        restaurantRepository.save(restaurant);
+        //dishService.create(dish);
+        return "redirect:/restaurant/restaurant/add"; // переход на форму ввода блюд
+
+    }
+
+}
+//rest
+/*
+@RestController
 @RequestMapping(value = "restaurant/restaurant/api/v1")
 public class RestaurantController {
 
@@ -30,7 +67,5 @@ public class RestaurantController {
     public Restaurant get(@PathVariable("id") long id) {
         return restaurantService.getById(id);
     } // поиск по id
-
-
-
-}*/
+}
+*/
