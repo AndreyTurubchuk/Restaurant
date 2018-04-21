e13d7d1f9f472e68882b2c3d02196600c593b7e5
Andrey Turubchuk <antur1977@rambler.ru>
2018-04-21 17:10:21 +0300

    edit DishController

--- restaurant/src/main/java/ru/topjava/restaurant/controller/DishController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/DishController.java
@@ -26,32 +26,68 @@ public class DishController {
     @Autowired
     private DishRepository dishRepository;
 
-/*    @GetMapping(value = "/dishes")
-    public List<Dish> dishes() {
-        return dishService.getAll();
-    } // лист*/
-
     @GetMapping(value = "/dishes")
+    public String dishList(Model model) {
+        model.addAttribute("dishList", dishRepository.findAll());
+        return "dishAll";
+    }
+
+    @GetMapping(value = "/dishesList")
     public List<Dish> dishes() {
         return dishRepository.findAll();
     } // лист
 
-    //@GetMapping(value = "/dish/add") //создание
-    @GetMapping(value = "/dish/add") //создание
-    public String dishAdd(Model model) {
+/*    @GetMapping(value = "/dish/create") //создание
+    public String dishCreate(Model model) {
         model.addAttribute("dish", new Dish());
         return "dishAddForm";
-        //return "test";
+    }*/
+
+    @GetMapping(value = "/dish/create") //создание
+    public String dishCreate(Model model) {
+        model.addAttribute("dish", new Dish());
+        return "dishForm";
     }
 
-    @PostMapping("/dish/add")
+    @GetMapping(value = "/dishes/{id}") //редактирование
+    public String dishEdit(Model model, @PathVariable long id) {
+        model.addAttribute("dish", dishRepository.getOne(id));
+        return "dishForm";
+    }
+
+    @PostMapping("/dishes/save") //post для create, update
+    public String dishSaveSubmit(@ModelAttribute Dish dish) {
+        dishRepository.save(dish);
+        return "redirect:/restaurant/dishes";
+    }
+
+/*
+    @PostMapping("/dish/create")
     public String dishSubmit(@ModelAttribute Dish dish) {
         dishRepository.save(dish);
-        //dishService.create(dish);
-        return "redirect:/restaurant/dish/add"; // переход на форму ввода блюд
-
+        return "redirect:/restaurant/dish/create"; // переход на форму ввода блюд
     }
+*/
 
+
+
+/*    @PostMapping(value = "/dishes/{id}") //обновление
+    public String update(@PathVariable(value = "id") long id, @RequestBody Dish dishDetails) {
+        Dish dish = dishRepository.getOne(id);
+        dish.setName(dishDetails.getName());
+        dish.setPrice(dishDetails.getPrice());
+        dishRepository.save(dish);
+        return "restaurant/dishes";
+    }*/
+
+
+
+
+    @DeleteMapping(value = "/dishes/{id}") // удаление
+    public String delete(@PathVariable("id") long id) {
+        dishRepository.delete(id);
+        return "restaurant/dishes";
+    }
 
 
 /*    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE) //создание
@@ -64,24 +100,21 @@ public class DishController {
         return dishService.getById(id);
     } // поиск по id
 
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE) //обновление
+/*    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE) //обновление
     public void update(@PathVariable("id") long id, @RequestBody Dish dish) {
         dishService.update(dish, id);
-    }
+    }*/
 
-    @GetMapping(value = "/")
+/*    @GetMapping(value = "/")
     public List<Dish> dishList() {
         Dish dish = new Dish("картошка", 3.0);
         dishService.create(dish);
         Dish dish2 = new Dish("картошка2", 32.0);
         dishService.create(dish2);
         return dishService.getAll();
-    }
+    }*/
 
-    @DeleteMapping("/{id}") // удаление
-    public void delete(@PathVariable("id") long id) {
-        dishService.delete(id);
-    }
+
 
 }
 
