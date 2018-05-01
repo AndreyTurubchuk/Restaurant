9a16d3ef169ba35281492f70bea964213893d298
Andrey Turubchuk <antur1977@rambler.ru>
2018-05-02 01:18:58 +0300

    add Restaurant Menu Repository

--- restaurant/src/main/java/ru/topjava/restaurant/repository/RestaurantMenuRepository.java
+++ restaurant/src/main/java/ru/topjava/restaurant/repository/RestaurantMenuRepository.java
@@ -1,10 +1,12 @@
 package ru.topjava.restaurant.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.topjava.restaurant.restaurantMenu.RestaurantMenu;
+import ru.topjava.restaurant.model.RestaurantMenu;
 
-/*@Repository
+
+@Repository
 public interface RestaurantMenuRepository extends JpaRepository<RestaurantMenu, Long> {
-    RestaurantMenu findRestaurantMenuById(Long id);
-}*/
+    //RestaurantMenu findRestaurantMenuById(Long id);
+}
