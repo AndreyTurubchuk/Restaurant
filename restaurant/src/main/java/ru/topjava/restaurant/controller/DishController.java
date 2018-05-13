7558e4a577c3c0fcf6652f73ecc6fcec6bc6c9ff
Andrey Turubchuk <antur1977@rambler.ru>
2018-05-14 01:01:19 +0300

    edit Dish Controller

--- restaurant/src/main/java/ru/topjava/restaurant/controller/DishController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/DishController.java
@@ -37,12 +37,6 @@ public class DishController {
         return dishRepository.findAll();
     } // лист
 
-/*    @GetMapping(value = "/dish/create") //создание
-    public String dishCreate(Model model) {
-        model.addAttribute("dish", new Dish());
-        return "dishAddForm";
-    }*/
-
     @GetMapping(value = "/dish/create") //создание
     public String dishCreate(Model model) {
         model.addAttribute("dish", new Dish());
@@ -61,60 +55,16 @@ public class DishController {
         return "redirect:/restaurant/dishes";
     }
 
-/*
-    @PostMapping("/dish/create")
-    public String dishSubmit(@ModelAttribute Dish dish) {
-        dishRepository.save(dish);
-        return "redirect:/restaurant/dish/create"; // переход на форму ввода блюд
-    }
-*/
-
-
-
-/*    @PostMapping(value = "/dishes/{id}") //обновление
-    public String update(@PathVariable(value = "id") long id, @RequestBody Dish dishDetails) {
-        Dish dish = dishRepository.getOne(id);
-        dish.setName(dishDetails.getName());
-        dish.setPrice(dishDetails.getPrice());
-        dishRepository.save(dish);
-        return "restaurant/dishes";
-    }*/
-
-
-
-
     @DeleteMapping(value = "/dishes/{id}") // удаление
     public String delete(@PathVariable("id") long id) {
         dishRepository.delete(id);
         return "restaurant/dishes";
     }
 
-
-/*    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE) //создание
-    public void create(@RequestBody Dish dish) {
-        dishService.create(dish);
-    }*/
-
     @GetMapping("/{id}")
     public Dish get(@PathVariable("id") long id) {
         return dishService.getById(id);
     } // поиск по id
-
-/*    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE) //обновление
-    public void update(@PathVariable("id") long id, @RequestBody Dish dish) {
-        dishService.update(dish, id);
-    }*/
-
-/*    @GetMapping(value = "/")
-    public List<Dish> dishList() {
-        Dish dish = new Dish("картошка", 3.0);
-        dishService.create(dish);
-        Dish dish2 = new Dish("картошка2", 32.0);
-        dishService.create(dish2);
-        return dishService.getAll();
-    }*/
-
-
 
 }
 
