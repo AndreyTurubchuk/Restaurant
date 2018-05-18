afa38339925b78ddc86ca822f4305733f38eac25
Andrey Turubchuk <antur1977@rambler.ru>
2018-05-18 10:51:33 +0300

    edit Rest Dish Controller

--- restaurant/src/main/java/ru/topjava/restaurant/controller/rest/RestDishController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/rest/RestDishController.java
@@ -3,6 +3,7 @@ package ru.topjava.restaurant.controller.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.topjava.restaurant.model.Dish;
 
@@ -24,104 +25,48 @@ public class RestDishController {
     @Autowired
     private DishRepository dishRepository;
 
-    @Autowired
-    private RestaurantRepository restaurantRepository;
-
-    @Autowired
-    private DishService dishService;
-
-    @Autowired
-    private RestaurantMenuRepository restaurantMenuRepository;
-
+    //список всех блюд для администратора
     @GetMapping("/dishes")
     public List<Dish> dishes() {
         return dishRepository.findAll();
     }
 
-    @GetMapping("/restaurants")
-    public List<Restaurant> restaurants() {
-        return restaurantRepository.findAll();
-    }
-
-    @GetMapping(value = "/menu") //отображение блюд одного ресторана с одним меню
-    public List<RestaurantMenu> menuForm() {
-        // List<RestaurantMenu> restaurantMenu = restaurantMenuRepository.findRestaurantMenuByRestaurantRestaurantId(restaurantId);
-        return restaurantMenuRepository.findAll();
-    }
-
-    @GetMapping(value = "{restaurantId}/menu") //отображение блюд одного ресторана с одним меню
-    public List<RestaurantMenu> menuByRestaurantId(@PathVariable("restaurantId") long id) {
-        return restaurantMenuRepository.findRestaurantMenuByRestaurantRestaurantId(id);
-    }
-
+    //выбор блюда по id
     @GetMapping("/dishes/{id}")
     public Dish get(@PathVariable("id") long id) {
         return dishRepository.findByDishId(id);
     }
 
-    @DeleteMapping(value = "/dishes/{id}") // удаление
+    // create a Dish
+    @PostMapping("/dishes/")
+    public void create(@RequestBody Dish dish) {
+        log.info("Creating Dish : {}", dish);
+        dishRepository.save(dish);
+    }
+
+    // update a Dish
+    @PutMapping("/dishes/{id}")
+    public void update(@RequestBody Dish dish, @PathVariable("id") long id) {
+        log.info("Updating Dish with id {}", id);
+        Dish currentDish = dishRepository.findByDishId(id);
+        if (currentDish == null) {
+            log.error("Unable to update. Dish with id {} not found. ", id);
+        } else {
+            currentDish.setName(dish.getName());
+            currentDish.setPrice(dish.getPrice());
+            dishRepository.save(currentDish);
+        }
+    }
+
+    // delete a Dish
+    @DeleteMapping(value = "/dishes/{id}")
     public void delete(@PathVariable("id") long id) {
         dishRepository.delete(id);
     }
 
-    @PutMapping("/dishes/{id}")
-    public void update(@RequestBody Dish dish, @PathVariable("id") long id) {
-        dishRepository.save(dish);
-    }
 
 
 
 
-/*    @PutMapping("/dishes/save")
-    public Dish update (@PathVariable("id") long id) {
-        return dishRepository.findByDishId(id);
-    }*/
-
-/*    @PutMapping("/dishes/{id}")
-    public void update (@RequestBody Dish dish, @PathVariable("id") long id) {
-        log.info("update {} with id={}", dish, id);
-        //  assureIdConsistent(dish, id);
-        dishService.update(dish);
-    }*/
-
-    @PutMapping("/dishes/save")
-    public void update(@RequestBody Dish dish) {
-        log.info("update {}", dish);
-        //  assureIdConsistent(dish, id);
-        dishService.update(dish);
-    }
-/*    @Override
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@RequestBody User user, @PathVariable("id") int id) {
-        super.update(user, id);
-    }*/
-
-
-/*
-    @PostMapping("/dishes")
-    public Dish dishSubmit(@ModelAttribute Dish dish) {
-        dishRepository.save(dish);
-        return dish;
-    }
-
-
-
-    @PutMapping(value = "/dishes/{id}") //обновление
-    public Dish update(@PathVariable long id, @RequestBody Dish dish) {
-        //Dish dish = dishRepository.getOne(id);
-        //dish.setName(dishDetails.getName());
-        //dish.setPrice(dishDetails.getPrice());
-        dishRepository.save(dish);
-        return dish;
-    }*/
-
-   /* @PutMapping(value = "/{id}") //обновление
-    public Dish update(@PathVariable long id, @RequestBody Dish dishDetails) {
-        Dish dish = dishRepository.getOne(id);
-        dish.setName(dishDetails.getName());
-        dish.setPrice(dishDetails.getPrice());
-        dishRepository.save(dish);
-        return dish;
-    }*/
 
 }
