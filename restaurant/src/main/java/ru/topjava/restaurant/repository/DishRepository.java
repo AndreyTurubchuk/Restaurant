9169457b1061097fe71cae4a9b3b2dc3b2c9c821
Andrey Turubchuk <antur1977@rambler.ru>
2018-05-21 01:57:49 +0300

    edit Dish Repository

--- restaurant/src/main/java/ru/topjava/restaurant/repository/DishRepository.java
+++ restaurant/src/main/java/ru/topjava/restaurant/repository/DishRepository.java
@@ -2,12 +2,21 @@ package ru.topjava.restaurant.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.topjava.restaurant.model.Dish;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Long> {
-    Dish findByDishId(Long id);
+    Dish findById(Long id);
+
+    @SuppressWarnings("JpaQlInspection")
+    @Query("SELECT d from Dish d  WHERE d.createdDate BETWEEN :startDate AND :endDate ORDER BY d.createdDate DESC")
+    List<Dish> getBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
