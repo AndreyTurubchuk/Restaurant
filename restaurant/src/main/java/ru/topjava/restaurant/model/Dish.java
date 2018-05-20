831428971a802c500e214806163cc1bdc1c7b926
Andrey Turubchuk <antur1977@rambler.ru>
2018-05-21 01:57:25 +0300

    add LocalDate Time by Dish

--- restaurant/src/main/java/ru/topjava/restaurant/model/Dish.java
+++ restaurant/src/main/java/ru/topjava/restaurant/model/Dish.java
@@ -2,6 +2,18 @@ package ru.topjava.restaurant.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/*@Entity
+@Table(name = "DEPARTMENTS")
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor*/
+
 
 @Entity
 @Table(name = "DISH")
@@ -9,25 +21,30 @@ public class Dish implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long dishId;
+    private long id;
 
     private String name;
     private Double price;
 
+    @Column(name = "CREATED_DATE")
+    //@Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdDate;
+
     public Dish() {
     }
 
-    public Dish(String name, Double price) {
+    public Dish(String name, Double price, LocalDateTime createdDate) {
         this.name = name;
         this.price = price;
+        this.createdDate = createdDate;
     }
 
-    public long getDishId() {
-        return dishId;
+    public long getId() {
+        return id;
     }
 
-    public void setDishId(long dishId) {
-        this.dishId = dishId;
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -46,12 +63,21 @@ public class Dish implements Serializable {
         this.price = price;
     }
 
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
     @Override
     public String toString() {
         return "Dish{" +
-                "dishId=" + dishId +
+                "id=" + id +
                 ", name='" + name + '\'' +
                 ", price=" + price +
+                ", createdDate=" + createdDate +
                 '}';
     }
 }
