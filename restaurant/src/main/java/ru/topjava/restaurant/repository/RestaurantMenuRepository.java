024b7005ab40207fe9ef78e5ed9c4f51a7ffa285
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-02 12:09:47 +0300

    edit Restaurant Nenu repository

--- restaurant/src/main/java/ru/topjava/restaurant/repository/RestaurantMenuRepository.java
+++ restaurant/src/main/java/ru/topjava/restaurant/repository/RestaurantMenuRepository.java
@@ -1,12 +1,9 @@
 package ru.topjava.restaurant.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.topjava.restaurant.model.RestaurantMenu;
-
 import java.util.List;
-
 
 @Repository
 public interface RestaurantMenuRepository extends JpaRepository<RestaurantMenu, Long> {
