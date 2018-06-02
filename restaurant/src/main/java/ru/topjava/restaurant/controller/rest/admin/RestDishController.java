5d9fbf6ee1f1622002c46ae2eb71850329d67387
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-02 12:10:16 +0300

    edit rest dish controller

--- restaurant/src/main/java/ru/topjava/restaurant/controller/rest/admin/RestDishController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/rest/admin/RestDishController.java
@@ -1,28 +1,16 @@
-package ru.topjava.restaurant.controller.rest;
+package ru.topjava.restaurant.controller.rest.admin;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.topjava.restaurant.model.Dish;
-
-import ru.topjava.restaurant.model.Restaurant;
-import ru.topjava.restaurant.model.RestaurantMenu;
 import ru.topjava.restaurant.repository.DishRepository;
-import ru.topjava.restaurant.repository.RestaurantMenuRepository;
-import ru.topjava.restaurant.repository.RestaurantRepository;
-import ru.topjava.restaurant.service.DishService;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
 import java.util.List;
 
 @RestController
-//@RequestMapping(value = "restaurant/dish/api/v1")
-@RequestMapping(value = "/restaurant/rest/admin/api/v1")
+@RequestMapping(value = "/admin/rest/api/v1")
 public class RestDishController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -35,7 +23,7 @@ public class RestDishController {
         return dishRepository.findAll();
     }
 
-   //выбор блюда по id
+    //выбор блюда по id
     @GetMapping("/dishes/{id}")
     public Dish get(@PathVariable("id") long id) {
         return dishRepository.findById(id);
@@ -48,6 +36,7 @@ public class RestDishController {
         dishRepository.save(dish);
     }
 
+    // update a Dish
     @PutMapping("/dishes/{id}")
     public void update(@RequestBody Dish dish, @PathVariable("id") long id) {
         log.info("Updating Dish with id {}", id);
@@ -63,6 +52,4 @@ public class RestDishController {
     public void delete(@PathVariable("id") long id) {
         dishRepository.delete(id);
     }
-
-
 }
