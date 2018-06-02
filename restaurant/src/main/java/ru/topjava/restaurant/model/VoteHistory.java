29e9a3a8a6685b4e51381bb7ed13ac7e7945a368
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-02 12:13:45 +0300

    add Vote History

---
+++ restaurant/src/main/java/ru/topjava/restaurant/model/VoteHistory.java
@@ -0,0 +1,37 @@
+package ru.topjava.restaurant.model;
+
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "VOTE_HISTORY")
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class VoteHistory implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "RESTAURANT_ID")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Restaurant restaurant;
+
+    @Column(name = "DATE_TIME_OF_VOTING")
+    private LocalDateTime dateTimeVote;
+}
