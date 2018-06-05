be3632b14ba91b55a0ad417efc7b22ae79947d95
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-05 17:18:32 +0300

    edit Restaurant menu

--- restaurant/src/main/java/ru/topjava/restaurant/model/RestaurantMenu.java
+++ restaurant/src/main/java/ru/topjava/restaurant/model/RestaurantMenu.java
@@ -21,49 +21,13 @@ public class RestaurantMenu implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long restaurantMenuId;
 
-    //@ManyToOne(fetch = FetchType.LAZY)
     @ManyToOne
     @JoinColumn(name = "DISH_ID")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Dish dish;
 
-    //@ManyToOne(fetch = FetchType.LAZY)
     @ManyToOne
     @JoinColumn(name = "RESTAURANT_ID")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Restaurant restaurant;
-
-  /*  public RestaurantMenu() {
-    }
-
-    public RestaurantMenu(Dish dish, Restaurant restaurant) {
-        this.dish = dish;
-     //   this.restaurant = restaurant;
-    }
-
-    public long getRestaurantMenuId() {
-        return restaurantMenuId;
-    }
-
-    public void setRestaurantMenuId(long restaurantMenuId) {
-        this.restaurantMenuId = restaurantMenuId;
-    }
-
-    public Dish getDish() {
-        return dish;
-    }
-
-    public void setDish(Dish dish) {
-        this.dish = dish;
-    }
-
-    public Restaurant getRestaurant() {
-        return restaurant;
-    }
-
-    public void setRestaurant(Restaurant restaurant) {
-        this.restaurant = restaurant;
-    }
-*/
-
 }
