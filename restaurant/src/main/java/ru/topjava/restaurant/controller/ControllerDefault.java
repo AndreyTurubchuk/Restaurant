5b013bd9136693cbbe4c8a91d09f44b391988ab7
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-05 17:16:41 +0300

    edit Controller default

--- restaurant/src/main/java/ru/topjava/restaurant/controller/ControllerDefault.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/ControllerDefault.java
@@ -1,6 +1,5 @@
 package ru.topjava.restaurant.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +10,4 @@ public class ControllerDefault {
     public String login() {
         return "Добро пожаловать";
     }
-/*
-
-    @GetMapping("/403")
-    public String error403() {
-        return "/error/403";
-    }*/
-
 }
