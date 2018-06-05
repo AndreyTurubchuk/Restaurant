5b5c4d7a1afb2e62d7297321aa33850c46afe7ec
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-05 17:17:24 +0300

    edit dish

--- restaurant/src/main/java/ru/topjava/restaurant/model/Dish.java
+++ restaurant/src/main/java/ru/topjava/restaurant/model/Dish.java
@@ -27,57 +27,7 @@ public class Dish implements Serializable {
     private Double price;
 
     @Column(name = "CREATED_DATE")
-    //@Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdDate;
 
-    /*public Dish() {
-    }
 
-    public Dish(String name, Double price, LocalDateTime createdDate) {
-        this.name = name;
-        this.price = price;
-        this.createdDate = createdDate;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Dish{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", createdDate=" + createdDate +
-                '}';
-    }*/
 }
