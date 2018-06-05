bca0c5cdae1b01b52f710bae58b6beb4eff5e465
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-05 17:19:52 +0300

    edit Vote controller

--- restaurant/src/main/java/ru/topjava/restaurant/controller/rest/user/VoteController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/rest/user/VoteController.java
@@ -17,6 +17,8 @@ import ru.topjava.restaurant.model.VoteHistory;
 import ru.topjava.restaurant.repository.DishRepository;
 import ru.topjava.restaurant.repository.RestaurantRepository;
 import ru.topjava.restaurant.repository.VoteHistoryRepository;
+import ru.topjava.restaurant.service.DishService;
+import ru.topjava.restaurant.service.VoteService;
 import ru.topjava.restaurant.util.DateTimeUtil;
 
 import java.time.LocalDate;
@@ -35,13 +37,54 @@ public class VoteController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    private RestaurantRepository restaurantRepository;
-
-    @Autowired
     private DishRepository dishRepository;
 
     @Autowired
     private VoteHistoryRepository voteHistoryRepository;
+
+    @Autowired
+    private DishService dishService;
+
+    @Autowired
+    private VoteService voteService;
+
+    //голосование за ресторан по id
+    @GetMapping("/restaurants/{id}/increase")
+    public String get(@PathVariable("id") long id) {
+        log.info("Vote user for restaurant {}", id);
+        return voteService.voteForRestaurant(id);
+    }
+
+    // было ли голосование сегодня
+    public boolean isVoteToday() {
+        log.info("whether there was a vote today");
+        return voteService.isVoteToday();
+
+    }
+
+    //список блюд на сегодня для пользователя если ресторан единственный и у него всего одно меню
+    @GetMapping("/dishesToday")
+    public List<Dish> getDishesToday() {
+        log.info("list of dishes for today");
+        return dishService.getDishesToday();
+    }
+
+    //список блюд на сегодня для пользователя у конкретного ресторана
+    @GetMapping("/restaurants/{id}/dishesToday")
+    public List<Dish> getDishesTodayForRestaurant(@PathVariable("id") long id) {
+        log.info("list of dishes for today for restaurant {}", id);
+        return dishService.getDishesForRestaurantToday(id);
+    }
+
+    //добавление блюда на сегодня  тест
+    @GetMapping("/dishAddToday")
+    public void addDishToday() {
+        Dish dish = new Dish();
+        dish.setCreatedDate(LocalDateTime.now());
+        dish.setName("milk");
+        dish.setPrice(200.0);
+        dishRepository.save(dish);
+    }
 
     @GetMapping("/getuser")
     public String user() {
@@ -56,7 +99,6 @@ public class VoteController {
         return voteHistory;
     }
 
-
     @GetMapping("/getVote")
     public String getVote() {
         List<VoteHistory> voteHistories = voteHistoryRepository.findVoteHistoriesByUsername("user");
@@ -69,83 +111,5 @@ public class VoteController {
         return "OK";
         //return voteHistoryRepository.findVoteHistoriesByUsername("user");
     }
-
-
-    //голосование за ресторан по id
-    @GetMapping("/restaurants/{id}/increase")
-    public String get(@PathVariable("id") long id) {
-        log.info("Vote user with restaurant {}", id);
-        String result;
-        Restaurant restaurant = restaurantRepository.findRestaurantByRestaurantId(id);
-        LocalDateTime startTime = LocalDate.now().atTime(LocalTime.MIDNIGHT);
-        LocalDateTime endTime = LocalDate.now().atTime(LocalTime.MAX);
-
-        LocalTime startTimeDay = LocalTime.MIN;
-        LocalTime endTimeDay = LocalTime.MAX;
-        LocalTime voteEndTime = LocalTime.of(11, 0, 0);
-        LocalTime current = LocalTime.now();
-
-        if (restaurant == null) {
-            log.error("Vote is impossible. Restaurant with id {} not found. ", id);
-            return "Vote is impossible. Restaurant not found";
-        }
-        if (DateTimeUtil.isBetween(LocalTime.now(), voteEndTime, endTimeDay)) {
-            result = "Голосование закрыто. Голосование возможно только с 00-00 до 11-00";
-        } else {
-            SecurityContext context = SecurityContextHolder.getContext();
-            String userName = context.getAuthentication().getName();
-
-            // уменьшение голоса у предыдущего ресторана
-            VoteHistory voteHistoryLast = voteHistoryRepository.findFirstByUsernameOrderByIdDesc(userName);
-            if (voteHistoryLast != null) {
-                long currentRaitingLast = voteHistoryLast.getRestaurant().getRating() - 1;
-                Restaurant restaurantLast = voteHistoryLast.getRestaurant();
-                restaurantLast.setRating(currentRaitingLast);
-                restaurantRepository.save(restaurantLast);
-            }
-
-            // увеличение голоса у текущего ресторана
-            VoteHistory voteHistory = new VoteHistory();
-            voteHistory.setRestaurant(restaurant);
-            voteHistory.setUsername(userName);
-            voteHistory.setDateTimeVote(LocalDateTime.now());
-            voteHistoryRepository.save(voteHistory);
-            long currentRaiting = restaurant.getRating() + 1;
-            restaurant.setRating(currentRaiting);
-            restaurantRepository.save(restaurant);
-            result = "OK";
-        }
-        return result;
-    }
-
-    private static void restaurantSave(Restaurant restaurant) {
-
-    }
-
-    //добавление блюда на сегодня
-    @GetMapping("/dishAddToday")
-    public void addDishToday() {
-        Dish dish = new Dish();
-        dish.setCreatedDate(LocalDateTime.now());
-        dish.setName("milk");
-        dish.setPrice(200.0);
-        dishRepository.save(dish);
-    }
-
-    //список блюд на сегодня для пользователя
-    @GetMapping("/dishesToday")
-    public List<Dish> getDishesToday() {
-        LocalDateTime startTime = LocalDate.now().atTime(LocalTime.MIDNIGHT);
-        LocalDateTime endTime = LocalDate.now().atTime(LocalTime.MAX);
-        List<Dish> dishes = dishRepository.findAll();
-        return dishes.stream()
-                .filter(dish -> DateTimeUtil.isBetween(dish.getCreatedDate(), startTime, endTime))
-                .collect(toList());
-    }
 }
 
-
-/*            LocalTime nowTime = LocalTime.now();
-            LocalTime startTimeDay = LocalTime.MIN;
-            LocalTime endTimeDay = LocalTime.MAX;
-            LocalTime voiteEndTime = LocalTime.of(11, 0, 0);*/
