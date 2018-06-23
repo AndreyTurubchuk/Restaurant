f76cc8a7811fe079056cea9fc44554d0a3864d69
Andrey Turubchuk <antur1977@rambler.ru>
2018-06-23 12:49:50 +0300

    edit dish repository

--- restaurant/src/main/java/ru/topjava/restaurant/repository/DishRepository.java
+++ restaurant/src/main/java/ru/topjava/restaurant/repository/DishRepository.java
@@ -7,14 +7,33 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.topjava.restaurant.model.Dish;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Long> {
     Dish findById(Long id);
 
-/*    @Query("SELECT d.id, d.createdDate, d.name, d.price FROM Dish d LEFT JOIN d.restaurantMenus AS rm WHERE rm.restaurant.restaurantId = :id")
+/*    @Query("select d.id, d.name, d.createdDate, d.price from Dish d LEFT JOIN d.restaurantMenus AS rm WHERE rm.restaurant.restaurantId = :id") // поиск блюд по номеру ресторана
     List<Dish> getDishesForRestaurant(
             @Param("id") Long id);*/
 
+/*    @Query("from Dish d LEFT JOIN d.restaurantMenus AS rm WHERE rm.restaurant.restaurantId = :id") // поиск блюд по номеру ресторана
+    List<Dish> getDishesForRestaurant(
+            @Param("id") Long id);*/
+
+  //  @Query("select d from Dish d LEFT JOIN d.restaurantMenus AS rm")
+        // поиск блюд по номеру ресторана
+   // List<Dish> getDishesForRestaurant();
+
+
 }
+   /* @Query("SELECT v from Voting v WHERE v.user.id=:userId AND v.operDate BETWEEN :startDate AND :endDate ORDER BY v.operDate DESC")
+    List<Voting> getBetweenDateByUser(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);*/
+
+/*    @Query("select d from Dish d LEFT JOIN d.restaurantMenus AS rm WHERE rm.restaurant.restaurantId = :id AND d.createdDate BETWEEN :startDate AND :endDate")
+        // поиск блюд по номеру ресторана
+    List<Dish> getDishesForRestaurant(
+            @Param("id") Long id,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);*/
