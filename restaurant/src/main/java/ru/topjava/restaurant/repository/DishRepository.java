06f84933d27a877d85897b3b1c074dc8d2ba6d9c
Andrey Turubchuk <antur1977@rambler.ru>
2018-07-13 12:52:58 +0300

    edit dish repository

--- restaurant/src/main/java/ru/topjava/restaurant/repository/DishRepository.java
+++ restaurant/src/main/java/ru/topjava/restaurant/repository/DishRepository.java
@@ -13,27 +13,5 @@ import java.util.List;
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Long> {
     Dish findById(Long id);
-
-/*    @Query("select d.id, d.name, d.createdDate, d.price from Dish d LEFT JOIN d.restaurantMenus AS rm WHERE rm.restaurant.restaurantId = :id") // поиск блюд по номеру ресторана
-    List<Dish> getDishesForRestaurant(
-            @Param("id") Long id);*/
-
-/*    @Query("from Dish d LEFT JOIN d.restaurantMenus AS rm WHERE rm.restaurant.restaurantId = :id") // поиск блюд по номеру ресторана
-    List<Dish> getDishesForRestaurant(
-            @Param("id") Long id);*/
-
-  //  @Query("select d from Dish d LEFT JOIN d.restaurantMenus AS rm")
-        // поиск блюд по номеру ресторана
-   // List<Dish> getDishesForRestaurant();
-
-
 }
-   /* @Query("SELECT v from Voting v WHERE v.user.id=:userId AND v.operDate BETWEEN :startDate AND :endDate ORDER BY v.operDate DESC")
-    List<Voting> getBetweenDateByUser(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);*/
 
-/*    @Query("select d from Dish d LEFT JOIN d.restaurantMenus AS rm WHERE rm.restaurant.restaurantId = :id AND d.createdDate BETWEEN :startDate AND :endDate")
-        // поиск блюд по номеру ресторана
-    List<Dish> getDishesForRestaurant(
-            @Param("id") Long id,
-            @Param("startDate") LocalDateTime startDate,
-            @Param("endDate") LocalDateTime endDate);*/
