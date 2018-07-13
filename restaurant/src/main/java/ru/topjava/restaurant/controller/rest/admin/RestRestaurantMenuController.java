19f9f6490e3b271461c0396cfd43b79e709ad6d0
Andrey Turubchuk <antur1977@rambler.ru>
2018-07-13 12:57:35 +0300

    edit rest restaurant menu controller

--- restaurant/src/main/java/ru/topjava/restaurant/controller/rest/admin/RestRestaurantMenuController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/rest/admin/RestRestaurantMenuController.java
@@ -4,9 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.topjava.restaurant.model.Dish;
+import ru.topjava.restaurant.model.Restaurant;
 import ru.topjava.restaurant.model.RestaurantMenu;
+import ru.topjava.restaurant.repository.DishRepository;
 import ru.topjava.restaurant.repository.RestaurantMenuRepository;
+import ru.topjava.restaurant.repository.RestaurantRepository;
+import ru.topjava.restaurant.service.RestaurantMenuService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -16,6 +22,15 @@ public class RestRestaurantMenuController {
 
     @Autowired
     private RestaurantMenuRepository restaurantMenuRepository;
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @Autowired
+    private DishRepository dishRepository;
+
+    @Autowired
+    private RestaurantMenuService restaurantMenuService;
 
     //список всех ресторанов с меню
     @GetMapping(value = "/menu")
@@ -30,11 +45,23 @@ public class RestRestaurantMenuController {
     }
 
     // create a Menu
-    @PostMapping("/menu/")
+    @PostMapping("/menu")
     public void create(@RequestBody RestaurantMenu restaurantMenu) {
-        log.info("Creating RestaurantMenu : {}", restaurantMenu);
+        log.info("creating RestaurantMenu : {}", restaurantMenu);
         restaurantMenuRepository.save(restaurantMenu);
     }
+
+    // update a Menu
+    @PutMapping("/menu/{id}")
+    public void update(@RequestBody RestaurantMenu restaurantMenu, @PathVariable("id") long id) {
+        log.info("Updating RestaurantMenu with id {}", id);
+        if (restaurantMenuRepository.findRestaurantMenuByRestaurantMenuId(id) == null) {
+            log.error("Unable to update. RestaurantMenu with id {} not found. ", id);
+        } else {
+            restaurantMenuRepository.save(restaurantMenu);
+        }
+    }
+
 
     // delete a Menu
     @DeleteMapping("/menu/{id}") // удаление
@@ -42,9 +69,34 @@ public class RestRestaurantMenuController {
         restaurantMenuRepository.delete(id);
     }
 
-    //отображение блюд одного ресторана с одним меню
-    @GetMapping(value = "/restaurant/{restaurantId}/menu") //отображение блюд одного ресторана с одним меню
-    public List<RestaurantMenu> menuByRestaurantId(@PathVariable("restaurantId") long id) {
+    //отображение всех блюд ресторана с меню за все дни
+    @GetMapping(value = "/restaurants/{id}/menu") //отображение блюд одного ресторана с одним меню
+    public List<RestaurantMenu> menuByRestaurantId(@PathVariable("id") long id) {
+        log.info("menu (list of dishes) by restaurant {}", id);
         return restaurantMenuRepository.findRestaurantMenuByRestaurantRestaurantId(id);
     }
+
+    //отображение меню (список блюд) ресторана на сегодня у конкретного ресторана
+    @GetMapping(value = "/restaurants/{id}/menuToday")
+    public List<RestaurantMenu> menuTodayByRestaurantId(@PathVariable("id") long id) {
+        log.info("menu (list of dishes) today by restaurant {}", id);
+        return restaurantMenuService.getMenuTodayByRestaurant(id);
+    }
+
+    // добавление блюда в меню ресторана
+    @GetMapping("/restaurants/{id}/dish/{dishId}/addDishByMenuByRestaurant")
+    public String addDishByMenuByRestaurant(@PathVariable("id") long id,
+                                               @PathVariable("dishId") long dishId) {
+        log.info("add Dish {}", dishId + " by Menu by restaurant {}", id);
+        RestaurantMenu rm = new RestaurantMenu();
+        //rm.setCreatedDate(LocalDateTime.now());
+        Restaurant restaurant = restaurantRepository.getOne(id);
+        Dish dish = dishRepository.getOne(dishId);
+        rm.setDish(dish);
+        rm.setRestaurant(restaurant);
+        restaurantMenuRepository.save(rm);
+        return "add dish " + dish.getId() + " in menu of restaurant " + restaurant.getRestaurantId() + " successfully";
+
+    }
+
 }
