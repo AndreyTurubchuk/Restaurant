86040ea3e67fec0cc49802d25d221e8f419f2291
Andrey Turubchuk <antur1977@rambler.ru>
2018-07-21 07:16:57 +0300

    edit rest dish controller

--- restaurant/src/main/java/ru/topjava/restaurant/controller/rest/admin/RestDishController.java
+++ restaurant/src/main/java/ru/topjava/restaurant/controller/rest/admin/RestDishController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.topjava.restaurant.model.Dish;
 import ru.topjava.restaurant.repository.DishRepository;
-
 import java.util.List;
 
 @RestController
